45debd18f322ede2d13962d68c41d8ac02aaacbc
JuliaTea <julia.tea@efrei.net>
2023-11-24 00:47:35 +0100

    Update tf.py

--- tf.py
+++ tf.py
@@ -4,7 +4,7 @@ tets="je suis un gros canard gros"
 #creer un dictionnaire vide
 tf={}
 #séparer chauqe mot du texte
-mots=texte.split()
+mots=tets.split()
 #ajouter dans le dictionnaire les mots s'il ne le sont pas déjà et indiquer leur occurence
 for mot in mots:
     if mot not in tf:
