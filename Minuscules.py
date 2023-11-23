b192e41b4b4080eb2a379b308366f3911f5474f4
AyunaChampain <151037671+AyunaChampain@users.noreply.github.com>
2023-11-23 15:00:37 +0100

    Fonction pour extraire les noms des fichiers

--- Minuscules.py
+++ Minuscules.py
@@ -1,17 +1,16 @@
-from os import *
+import os
 
-def minuscules() :
-    with open("speeches/Nomination_Macron.txt", "r") as f :
+def minuscules(file) :
+
+    with open(file, 'r') as f :
         contenu = f.readlines()
-    for line in contenu :
-        new_line = ""
-        for cara in line :
-            if ord(cara) >= 65 and ord(cara) <= 90 :
-                new_line += chr(ord(cara) + 32)
-            else :
-                new_line += cara
-        with open("file.txt", "a") as f2 :
-            f2.write(new_line)
+        for line in contenu :
+            new_line = ""
+            for cara in line :
+                if ord(cara) >= 65 and ord(cara) <= 90 :
+                    new_line += chr(ord(cara) + 32)
+                else :
+                    new_line += cara
+            with open("file.txt", "a") as f2 :
+                f2.write(new_line)
 
-
-minuscules()
