103bddc43c6649892203517f164adae0497d897b
orpheeteyssedre <151037799+orpheeteyssedre@users.noreply.github.com>
2023-11-24 12:52:23 +0100

    le plus repeté

--- tf-idf_elevé.py
+++ tf-idf_elevé.py
@@ -1,12 +1,18 @@
 #pour obtenir la liste des mots dans l’ordre dans lequel ils apparaissent dans la matrice TD-IDF.
 #obtenir le score TD-IDF le plus élevé pour chaque document.
+""""
 for i in range(len(files)):
     max_tfidf = tfidf[i].max()
-    print(f"Le score TD-IDF le plus élevé pour le fichier {files[i]} est {max_tfidf}.")
-def tfidf_eleve(tfidf, listemots):
+    print(f"Le score TD-IDF le plus élevé pour le fichier {files[i]} est {max_tfidf}.")""""
+def mot_eleve(tfidf, listemots):
     maxi_tfidf2 = 0
+    lePlusRepete = ""
     #parcourir la ligne de la matrice
     for i in range(len(tfidf)):
         #parcourir colone de la matrice
         for j in range(len(tfidf[i])):
+            ## Vérifier si le score tf idf est plus grand que le maximum actuel
             if tfidf[i][j] > maxi_tfidf2:
+                maxi_tfidf2 = tfidf[i][j]
+                lePlusRepete = listemots[i]
+    return lePlusRepete
