eed5db27df13a81e3bf38f695b82f59b71a06cf1
AyunaChampain <151037671+AyunaChampain@users.noreply.github.com>
2023-11-24 09:23:22 +0100

    petit changement

--- tf.py
+++ tf.py
@@ -1,15 +1,23 @@
+from math import *
+from os import listdir
 
-test="je suis un gros canard gros"
+def list_of_files(directory, extension):
+    files_names = []
+    for filename in listdir(directory):
+        if filename.endswith(extension):
+            files_names.append(filename)
+    return files_names
 
-#creer un dictionnaire vide
-tf={}
-#séparer chaque mot du texte
-mots=test.split()
-#ajouter dans le dictionnaire les mots s'il ne le sont pas déjà et indiquer leur occurence
-for mot in mots:
-    if mot not in tf:
-        tf[mot]=1
-    else:
-        tf[mot] = tf[mot] + 1
-print(tf)
+def tf (repert) :
+    #creer un dictionnaire vide
+    tf={}
+    #séparer chaque mot du texte
+    mots=test.split()
+    #ajouter dans le dictionnaire les mots s'il ne le sont pas déjà et indiquer leur occurence
+    for mot in mots:
+        if mot not in tf:
+            tf[mot]=1
+        else:
+            tf[mot] = tf[mot] + 1
+    return tf
 
