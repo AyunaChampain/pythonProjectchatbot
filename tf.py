05c245b24c402d1d13bb22fdff0fbcd7ca60a846
JuliaTea <julia.tea@efrei.net>
2023-11-24 09:30:08 +0100

    modif

--- tf.py
+++ tf.py
@@ -1,3 +1,4 @@
+
 from math import *
 from os import listdir
 
@@ -8,16 +9,26 @@ def list_of_files(directory, extension):
             files_names.append(filename)
     return files_names
 
-def tf (repert) :
-    #creer un dictionnaire vide
-    tf={}
-    #séparer chaque mot du texte
-    mots=test.split()
-    #ajouter dans le dictionnaire les mots s'il ne le sont pas déjà et indiquer leur occurence
-    for mot in mots:
-        if mot not in tf:
-            tf[mot]=1
-        else:
-            tf[mot] = tf[mot] + 1
-    return tf
 
+def tf (repert) :
+    dico = {}
+    M = []
+    list = list_of_files(repert, "txt")
+    for i in range (len(list)) :
+        L = []
+        with open("./cleaned/" + list[i], "r") as f :
+            Lignes = f.readlines()
+    for discours in Lignes:
+        #creer un dictionnaire vide
+        tf={}
+        #séparer chaque mot du texte
+        mots=discours.split()
+        #ajouter dans le dictionnaire les mots s'il ne le sont pas déjà et indiquer leur occurence
+        for mot in mots:
+            if mot not in tf:
+                tf[mot]=1
+            else:
+                tf[mot] = tf[mot] + 1
+        print(tf)
+
+tf("./cleaned")
