327f036f38244f2357287b3fec0fc4f2762bfc6b
orpheeteyssedre <151037799+orpheeteyssedre@users.noreply.github.com>
2023-11-24 09:12:58 +0100

    fgtdhsk

---
+++ tf-idf_elevé.py
@@ -0,0 +1,12 @@
+#pour obtenir la liste des mots dans l’ordre dans lequel ils apparaissent dans la matrice TD-IDF.
+#obtenir le score TD-IDF le plus élevé pour chaque document.
+for i in range(len(files)):
+    max_tfidf = tfidf[i].max()
+    print(f"Le score TD-IDF le plus élevé pour le fichier {files[i]} est {max_tfidf}.")
+def tfidf_eleve(tfidf, listemots):
+    maxi_tfidf2 = 0
+    #parcourir la ligne de la matrice
+    for i in range(len(tfidf)):
+        #parcourir colone de la matrice
+        for j in range(len(tfidf[i])):
+            if tfidf[i][j] > maxi_tfidf2:
