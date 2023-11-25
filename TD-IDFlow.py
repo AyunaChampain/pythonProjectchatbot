d082d5a07cbe7299c94cb046263db05318d9798a
AyunaChampain <151037671+AyunaChampain@users.noreply.github.com>
2023-11-25 13:00:15 +0100

    TF-IDF

--- TD-IDFlow.py
+++ TD-IDFlow.py
@@ -1,15 +1,17 @@
 from TFIDF import *
 
 TfIdf = TFIDF("./cleaned")
-print(TfIdf)
 min = 0
-
+mot = []
+print(TfIdf)
 
 for i in range(len(TfIdf)) :
     s = 0
     for j in range(1,len(TfIdf[i])) :
         s += TfIdf[i][j]
-    if s >= min :
+    s /= len(TfIdf[i])
+    print(s)
+    if s <= 0.35 :
         min = s
-        mot = TfIdf[i][0]
+        mot.append(TfIdf[i][0])
 print(mot)
