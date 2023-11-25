f04c166c37de12dff5ba58c24e467e399f1ff3ec
AyunaChampain <151037671+AyunaChampain@users.noreply.github.com>
2023-11-25 18:48:07 +0100

    Correction

--- tfidfelevé.py
+++ tfidfelevé.py
@@ -1,5 +1,7 @@
 #pour obtenir la liste des mots dans l’ordre dans lequel ils apparaissent dans la matrice TD-IDF.
 #obtenir le score TD-IDF le plus élevé pour chaque document.
+#files ?
+
 for i in range(len(files)):
     max_tfidf = tfidf[i].max()
     print(f"Le score TD-IDF le plus élevé pour le fichier {files[i]} est {max_tfidf}.")
@@ -12,5 +14,3 @@ def tfidf_eleve(tfidf, listemots):
             if tfidf[i][j] > maxi_tfidf2:
                 maxi_tfidf2 = tfidf[i][j]
     return tfidf
-
-#?
