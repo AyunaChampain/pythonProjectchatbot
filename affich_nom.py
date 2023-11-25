043d149b513812201427b73bd7d72f9e646b01c3
AyunaChampain <151037671+AyunaChampain@users.noreply.github.com>
2023-11-25 14:49:27 +0100

    Correction

--- affich_nom.py
+++ affich_nom.py
@@ -8,20 +8,20 @@ def nompresident(noms_prsdt):
     cptmitterrand = 0
     for nom in noms_prsdt:
         if nom == 'Chirac' and cptchirac == 0 :
-            return(nom, end = " ")
+            return(nom)
             cptchirac = 1
         elif nom == 'Mitterrand' and cptmitterrand == 0 :
-            return(nom, end =" ")
+            return(nom)
             cptmitterrand = 1
         elif nom == 'Hollande' and cpthollande == 0 :
-            return(nom, end =" ")
+            return(nom)
             cpthollande = 1
         elif nom == 'Macron' and cptmacron == 0 :
-            return(nom, end =" ")
+            return(nom)
             cptmacron = 1
         elif nom == 'Sarkozy' and cptsarkozy == 0 :
-            return(nom, end=" ")
+            return(nom)
             cptsarkozy = 1
         elif nom == 'Giscard dEstaing' and cptgiscard == 0 :
-            return(nom, end=" ")
+            return(nom)
             cptgidcard = 1
