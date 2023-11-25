0a055fde0fc95bb37f1224cefed90abf19644292
JuliaTea <julia.tea@efrei.net>
2023-11-25 17:42:27 +0100

    modif

--- affich_nom.py
+++ affich_nom.py
@@ -1,5 +1,7 @@
 # il faudra mettre les noms des présidents dans une liste listedoc
+noms_prsdt=['Chirac', 'Chirac', 'Giscard dEstaing', 'Hollande', 'Macron', 'Mitterrand', 'Mitterrand', 'Sarkozy']
 def nompresident(noms_prsdt):
+    Listepres=[]
     #cpt pour n'afficher que la première occurence
     cpthollande = 0
     cptchirac = 0
@@ -9,20 +11,22 @@ def nompresident(noms_prsdt):
     cptmitterrand = 0
     for nom in noms_prsdt:
         if nom == 'Chirac' and cptchirac == 0 :
-            return(nom, end= " ")
+            Listepres.append(nom)
             cptchirac = 1
         elif nom == 'Mitterrand' and cptmitterrand == 0 :
-            return(nom, end=" ")
+            Listepres.append(nom)
             cptmitterrand = 1
         elif nom == 'Hollande' and cpthollande == 0 :
-            return(nom, end=" ")
+            Listepres.append(nom)
             cpthollande = 1
         elif nom == 'Macron' and cptmacron == 0 :
-            return(nom, end=" ")
+            Listepres.append(nom)
             cptmacron = 1
         elif nom == 'Sarkozy' and cptsarkozy == 0 :
-            return(nom, end=" ")
+            Listepres.append(nom)
             cptsarkozy = 1
         elif nom == 'Giscard dEstaing' and cptgiscard == 0 :
-            return(nom, end=" ")
+            Listepres.append(nom)
             cptgidcard = 1
+    return Listepres
+
