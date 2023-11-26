b3dcb1e7e7688fc36da2aee9a8f51e5df7e5a00d
orpheeteyssedre <151037799+orpheeteyssedre@users.noreply.github.com>
2023-11-26 17:36:13 +0100

    find commun commentaire

--- find_commun.py
+++ find_commun.py
@@ -3,13 +3,15 @@ tf = tf("./cleaned")
 TfIdf = TFIDF("./cleaned")
 mot = []
 
-
+#liste pour ajouter tout les mots utilisés par chacun des présidents
 tousmots = []
 for i in tf[0] :
     compt = 0
+    #parcour le dictionnaire
     for j in range (len(tf)) :
         if i in tf[j] :
             compt += 1
+    #si le compteur = à 8 un des mots à étè utilisé dans chaque fichier, il est donc ajouté à une liste
     if compt == 8 :
         tousmots.append(i)
 print("Les mots que tous les présidents ont prononcé sont : ", tousmots)
