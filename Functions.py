8f2af49b30f5c923d258383f517c340cabc5ac39
JuliaTea <julia.tea@efrei.net>
2023-11-26 15:56:29 +0100

    modif

--- Functions.py
+++ Functions.py
@@ -23,30 +23,24 @@ def nompresident(noms_prsdt):
     cptmitterrand = 0
     for nom in noms_prsdt:
         if nom == 'Chirac' and cptchirac == 0 :
-            return(nom, end= " ")
             Listepres.append(nom)
             cptchirac = 1
         elif nom == 'Mitterrand' and cptmitterrand == 0 :
-            return(nom, end=" ")
             Listepres.append(nom)
             cptmitterrand = 1
         elif nom == 'Hollande' and cpthollande == 0 :
-            return(nom, end=" ")
             Listepres.append(nom)
             cpthollande = 1
         elif nom == 'Macron' and cptmacron == 0 :
-            return(nom, end=" ")
             Listepres.append(nom)
             cptmacron = 1
         elif nom == 'Sarkozy' and cptsarkozy == 0 :
-            return(nom, end=" ")
             Listepres.append(nom)
             cptsarkozy = 1
         elif nom == 'Giscard dEstaing' and cptgiscard == 0 :
-            return(nom, end=" ")
             Listepres.append(nom)
             cptgidcard = 1
-    return Listepres
+    return(Listepres)
 
 
 #Fonction isolant le nom du président du nom du fichier
@@ -177,5 +171,5 @@ def listemots(tf):
             a.append(cle)
     print(a)
 
-var = tf("./cleaned")
-listemots(var)
+"""var = tf("./cleaned")
+listemots(var)"""
