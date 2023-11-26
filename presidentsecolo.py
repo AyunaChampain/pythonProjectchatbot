2bd2fda27234dbff8274a5d0c7b8c340b8be8ed8
orpheeteyssedre <151037799+orpheeteyssedre@users.noreply.github.com>
2023-11-26 15:39:15 +0100

    question ligne 12

--- presidentsecolo.py
+++ presidentsecolo.py
@@ -9,7 +9,7 @@ def ecologie_climat(mots, liste_files):
             # Lire le contenu du fichier
             with open(filenoms, 'r', encoding='utf-8') as file: #changer le with??
                 texte = file.read().split() #créer une liste des mots des fichiers
-    for i in len(texte): #parcour les element de la liste
+    for i in len(texte):  #// for i texte        #parcour les element de la liste
         if texte[i] in mots: #vérifie si le mot et un des deux mots cherché
             index = i
             liste_index = liste_index.append(i) #ajoute à une liste a quelle indexe un des mots et apparus
