1f89e8efbad7037f3e304a3a16be67e6a2c61b0c
orpheeteyssedre <151037799+orpheeteyssedre@users.noreply.github.com>
2023-12-07 16:03:09 +0100

    prod scalaire

--- prod_scal.py
+++ prod_scal.py
@@ -2,5 +2,5 @@
 def prod_scal():
     sommeAB = 1
     for i in range(len(A)):  #//len(M)
-        sommeAB = A[i]*B[i]  #pck B c'est une ligne nan?
+        sommeAB += A[i]*B[i]  #pck B c'est une ligne nan?
     return sommeAB
