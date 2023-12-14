86f0b3acdf3c41ba7ae79a79be13cb1cfe288a43
AyunaChampain <151037671+AyunaChampain@users.noreply.github.com>
2023-12-14 16:38:51 +0100

    Ajout premiers commentaires (noms)

--- ChatBot.py
+++ ChatBot.py
@@ -6,11 +6,11 @@ from reponsereturn import *
 from qu5 import *
 from tf_eleve_question import *
 
-question = str(input("Bonjour cher utilisateur ! Je suis JOC, ChatBot à votre service, comment puis-je vous aider ? "))
+question = str(input("Bonjour cher utilisateur ! Je suis J.O.C., ChatBot à votre service, comment puis-je vous aider ? "))
 question = mots_question(question)
 tfidf = inverse_matr(TFIDF("./cleaned"))
 TFIDF_qu = Tfidf(question)
 files = list_of_files("./cleaned", 'txt')
 doc = pertin(tfidf, TFIDF_qu, files)
-mot = tfidf_question(question)
+mot = "nation"
 print(repreturn(doc, mot))
--- reponsereturn.py
+++ reponsereturn.py
@@ -2,17 +2,13 @@
 # Projet Python : My First ChatBot
 # Fonction qui retourne la phrase contenant la première occurence du mot au TF-IDF le plus élevé dans le bon document
 def repreturn(docfichier,motquestioneleve):
+    docpertinent = docpertinent.replace("-",".")
+    docpertinent = docpertinent.replace("!",".")
+    docpertinent = docpertinent.replace("?",".")
     with open("docfichier","w") as docpertinent:
-        docpertinent = docpertinent.replace("-",".")
-        docpertinent = docpertinent.replace("!",".")
-        docpertinent = docpertinent.replace("?",".")
         a = docpertinent.split(".")
         cpt = 0
         for sentence in a:
             if motQuestionEleve in a and cpt == 0:
                 cpt = 1
                 return a
-
-
-
-
