407466576a42672502c08276ec585efc90680b16
orpheeteyssedre <151037799+orpheeteyssedre@users.noreply.github.com>
2023-12-14 15:43:07 +0100

    tf eleve question

--- tf_eleve_question.py
+++ tf_eleve_question.py
@@ -1,15 +1,15 @@
 # Champain Ayuna, Tea Julia, Teyssedre Orphée --- Groupe C
 # Projet Python : My First ChatBot
-# Fonction
 from score_qu import *
 from motsquestion import *
+listedesmots = mots_question(question)
 tf_question = tf_qu(quest)
 def tfidf_question():
     eleve = 0
     place = 0
-    mot_question = " "
-    for i in len(tf_question):
+    for i in range(len(tf_question)):
         if tf_question[i] >= eleve:
             eleve = tf_question[i]
             place = i
-    mot_question = listedesmots[place]
+    motquestioneleve = listedesmots[place]
+    return motquestioneleve
