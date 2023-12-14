9f2aa8d770e9c2fd575b141abff728255d563a27
orpheeteyssedre <151037799+orpheeteyssedre@users.noreply.github.com>
2023-12-14 14:49:27 +0100

    avancement tf question

--- tf_eleve_question.py
+++ tf_eleve_question.py
@@ -1,11 +1,10 @@
 #Dans le vecteur TF-IDF de la question,
 # repérer le mot ayant le score TF-IDF le plus élevé et le retourner.
 from score_qu import *
-tf_question =
+tf_question = tf_qu
 def tfidf_question():
     eleve = 0
     for i in tf_question:
         if i >= eleve:
             eleve = i
-
-
+    tf_qestion[eleve]
