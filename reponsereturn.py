e1d61e82647e2b9e7881bc894fadd1111fc13769
AyunaChampain <151037671+AyunaChampain@users.noreply.github.com>
2023-12-14 16:41:58 +0100

    Ajout premiers commentaires (noms)

--- reponsereturn.py
+++ reponsereturn.py
@@ -1,11 +1,12 @@
 # Champain Ayuna, Tea Julia, Teyssedre Orphée --- Groupe C
 # Projet Python : My First ChatBot
 # Fonction qui retourne la phrase contenant la première occurence du mot au TF-IDF le plus élevé dans le bon document
-def repreturn(docfichier,motquestioneleve):
-    docpertinent = docpertinent.replace("-",".")
-    docpertinent = docpertinent.replace("!",".")
-    docpertinent = docpertinent.replace("?",".")
+def repreturn(docpertinent,motquestioneleve):
+    print(docpertinent)
     with open("docfichier","w") as docpertinent:
+        docpertinent = docpertinent.replace("-",".")
+        docpertinent = docpertinent.replace("!",".")
+        docpertinent = docpertinent.replace("?",".")
         a = docpertinent.split(".")
         cpt = 0
         for sentence in a:
