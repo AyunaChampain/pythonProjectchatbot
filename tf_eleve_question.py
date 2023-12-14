4333e655aa72e9cda0d242e6131e121c61ac7d65
orpheeteyssedre <151037799+orpheeteyssedre@users.noreply.github.com>
2023-12-14 15:28:38 +0100

    finis mots eleve question

--- tf_eleve_question.py
+++ tf_eleve_question.py
@@ -1,3 +1,5 @@
+# Champain Ayuna, Tea Julia, Teyssedre Orphée --- Groupe C
+# Projet Python : My First ChatBot
 from score_qu import *
 from motsquestion import *
 listedesmots = mots_question(question)
