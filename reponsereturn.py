c001bd86017767361d80d63cf92025dbd8542eba
JuliaTea <julia.tea@efrei.net>
2023-12-14 16:35:59 +0100

    ajouts de commentaires

--- reponsereturn.py
+++ reponsereturn.py
@@ -1,16 +1,17 @@
 # Champain Ayuna, Tea Julia, Teyssedre Orphée --- Groupe C
 # Projet Python : My First ChatBot
 # Fonction qui retourne la phrase contenant la première occurence du mot au TF-IDF le plus élevé dans le bon document
-def repreturn(docpertinent,motquestioneleve):
-    docpertinent = docpertinent.replace("-",".")
-    docpertinent = docpertinent.replace("!",".")
-    docpertinent = docpertinent.replace("?",".")
-    a = docpertinent.split(".")
-    cpt = 0
-    for sentence in a:
-        if motQuestionEleve in a and cpt == 0:
-            cpt = 1
-            return a
+def repreturn(docfichier,motquestioneleve):
+    with open("docfichier","w") as docpertinent:
+        docpertinent = docpertinent.replace("-",".")
+        docpertinent = docpertinent.replace("!",".")
+        docpertinent = docpertinent.replace("?",".")
+        a = docpertinent.split(".")
+        cpt = 0
+        for sentence in a:
+            if motQuestionEleve in a and cpt == 0:
+                cpt = 1
+                return a
 
 
 
