6b100b94fef201822d48c97cba04958bce86b447
orpheeteyssedre <151037799+orpheeteyssedre@users.noreply.github.com>
2023-12-14 15:27:22 +0100

    finis mots eleve question

--- tf_eleve_question.py
+++ tf_eleve_question.py
@@ -1,12 +1,13 @@
 from score_qu import *
 from motsquestion import *
+listedesmots = mots_question(question)
 tf_question = tf_qu(quest)
 def tfidf_question():
     eleve = 0
     place = 0
-    mot_question = " "
-    for i in len(tf_question):
+    for i in range(len(tf_question)):
         if tf_question[i] >= eleve:
             eleve = tf_question[i]
             place = i
-    mot_question = listedesmots[place]
+    motquestioneleve = listedesmots[place]
+    return motquestioneleve
