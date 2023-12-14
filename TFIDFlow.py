2ea6012ea771f9ad57679bdb195fbb38b636b507
JuliaTea <julia.tea@efrei.net>
2023-12-14 15:53:19 +0100

    ajouts de commentaires

--- TFIDFlow.py
+++ TFIDFlow.py
@@ -1,5 +1,6 @@
 # Champain Ayuna, Tea Julia, Teyssedre Orphée --- Groupe C
 # Projet Python : My First ChatBot
+# Fonction qui affiche les mots au score TF-IDF de 0
 
 from Functions import *
 
@@ -15,7 +16,7 @@ def TFIDFLOW( ) :
         #Calcule la moyenne des scores TfIdf de chaque mot
         for j in range(1,len(TfIdf[i])) :
             s += TfIdf[i][j]
-        #Garde les mots dont le score est inférieur à 0.38
+        #Garde les mots dont le score est de 0
         if s == 0 :
             mot.append(TfIdf[i][0])
     return mot
