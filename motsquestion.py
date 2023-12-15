017c537a9d56b5e04263df33f050c4632819edac
JuliaTea <julia.tea@efrei.net>
2023-12-15 09:52:41 +0100

    mots mot mo

--- motsquestion.py
+++ motsquestion.py
@@ -3,19 +3,19 @@
 # Retourne une liste des mots de la question en enlevant les ponctuations et majuscules
 def mots_question(question):
     listedesmots=[]
-    mots = question.split()
-    mot=''
+    mot = question.split()
+    mots=''
     for cara in question:
         if ord('A') <= ord(cara) <= ord('Z'):
-            mot += chr(ord(cara) + 32)
+            mots += chr(ord(cara) + 32)
         elif 31 >= ord(cara) >= 0 or 33 <= ord(cara) <= 45 or 47 <= ord(cara) <= 64 or 91>= ord(cara) >= 96 or 127 >= ord(cara)>= 123:
-            mot += ''
+            mots += ''
         elif ord(cara) == 32 or ord(cara) == 46:
-            mot += ' '
+            mots += ' '
         else:
-            mot += chr(ord(cara))
-        mots = mot.split()
-    for mo in mots:
+            mots += chr(ord(cara))
+        mot = mots.split()
+    for mo in mot:
         if mo not in listedesmots and mo != " " and mo != "":
             listedesmots.append(mo)
     return listedesmots
