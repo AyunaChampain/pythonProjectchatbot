ee6a98c8c3d6e93ca0a53800d9261ce4944ba372
AyunaChampain <151037671+AyunaChampain@users.noreply.github.com>
2023-12-16 10:50:17 +0100

    Ajout premiers commentaires (noms)

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ if __name__ == '__main__':
         print("3 pour les mots les plus répétés par le président Chirac.")
         print("4 pour obtenir la liste des noms des présidents ayant parlé de Nation et celui qui a répété le plus de fois le mot Nation.")
         print("5 pour obtenir le nom du premier président à parler d'écologie.")
-        print("6 pour obtenir les mots que tous les présidents ont prononcé.")
         choix = 0
         while choix < 1 or choix > 6 :
             choix = int(input("Choisissez une commande : "))
@@ -41,5 +40,3 @@ if __name__ == '__main__':
             from findpresidentnation import *
         if choix == 5 :
             from findecolo import *
-        if choix == 6 :
-            from find_commun import *
--- motsquestion.py
+++ motsquestion.py
@@ -1,6 +1,7 @@
 # Champain Ayuna, Tea Julia, Teyssedre Orphée --- Groupe C
 # Projet Python : My First ChatBot
 # Retourne une liste des mots de la question en enlevant les ponctuations et majuscules
+
 def mots_question(question):
     listedesmots=[]
     mots = question.split()
@@ -9,7 +10,7 @@ def mots_question(question):
         if ord('A') <= ord(cara) <= ord('Z'):
             mot += chr(ord(cara) + 32)
         elif 31 >= ord(cara) >= 0 or 33 <= ord(cara) <= 45 or 47 <= ord(cara) <= 64 or 91>= ord(cara) >= 96 or 127 >= ord(cara)>= 123:
-            mot += ''
+            mot += ' '
         elif ord(cara) == 32 or ord(cara) == 46:
             mot += ' '
         else:
