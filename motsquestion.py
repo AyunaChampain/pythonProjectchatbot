f0301aa2c733991c9004326d005561a7467d496b
AyunaChampain <151037671+AyunaChampain@users.noreply.github.com>
2023-12-16 10:51:05 +0100

    Ajout premiers commentaires (noms)

--- motsquestion.py
+++ motsquestion.py
@@ -9,7 +9,7 @@ def mots_question(question):
         if ord('A') <= ord(cara) <= ord('Z'):
             mots += chr(ord(cara) + 32)
         elif 31 >= ord(cara) >= 0 or 33 <= ord(cara) <= 45 or 47 <= ord(cara) <= 64 or 91>= ord(cara) >= 96 or 127 >= ord(cara)>= 123:
-            mots += ''
+            mots += ' '
         elif ord(cara) == 32 or ord(cara) == 46:
             mots += ' '
         else:
