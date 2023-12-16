ffdb6ad7aac28b9cc5c296b87483cada71b0cc69
AyunaChampain <151037671+AyunaChampain@users.noreply.github.com>
2023-12-16 11:17:13 +0100

    .

--- qu5.py
+++ qu5.py
@@ -9,7 +9,6 @@ def pertin(tfidf, TFIDF_qu, files) :
     max = 1
     for i in range (len(tfidf)) :
         a = sim(tfidf[i], TFIDF_qu)
-        print(a)
         if a >= max :
             max = a
             nom = files[i]
